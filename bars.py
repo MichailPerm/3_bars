f010c284f03607fb0fcd19c426b477905a182e68
Michail_Z <apxahrejl@gmail.com>
2018-05-01 01:24:25 +0500

    Added coordinates validation

--- bars.py
+++ bars.py
@@ -6,12 +6,12 @@ import sys
 
 def create_args_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("filepath")
+    parser.add_argument('filepath')
     return parser.parse_args()
 
 
 def load_data(filepath):
-    with open(filepath, "r") as file:
+    with open(filepath, 'r') as file:
         dict_content = json.load(file)
         return dict_content['features']
 
@@ -39,17 +39,25 @@ if __name__ == '__main__':
     try:
         dicts_list = load_data(args.filepath)
     except json.decoder.JSONDecodeError:
-        print("File {} does not contain json data.".format(args.filepath))
+        print('Файл {} не содержит данных json.'.format(args.filepath))
     except IOError:
-        print("Unable to read file {}.".format(args.filepath))
+        print('Невозможно прочитать данные {}.'.format(args.filepath))
     else:
         if not dicts_list:
-            sys.exit("Program finished it's work, becasue of None json data.")
+            sys.exit('Программа завершила работу из-за отсутствия данных json.')
         else:
             the_biggest_bar = get_biggest_bar(dicts_list)
             the_smallest_bar = get_smallest_bar(dicts_list)
             longitude = input('Введите долготу: ')
             latitude = input('Введите широту: ')
+            try:
+                float_longitude = float(longitude)
+            except (ValueError, NameError):
+                sys.exit('Программа завершила работу из-за некорректно введенной долготы')
+            try:
+                float_latitude = float(latitude)
+            except (ValueError, NameError):
+                sys.exit('Программа завершила работу из-за некорректно введенной широты')
             the_closest_bar = get_closest_bar(dicts_list, longitude, latitude)
             print('Самый маленький бар - {}'.format(
                 the_smallest_bar['properties']['Attributes']['Name']))
