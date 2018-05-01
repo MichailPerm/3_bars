8dc3ab188f9ade76fa016a4189031aa064ac14d0
Michail_Z <apxahrejl@gmail.com>
2018-05-01 12:17:58 +0500

    Refactoring

--- bars.py
+++ bars.py
@@ -16,52 +16,63 @@ def load_data(filepath):
         return dict_content['features']
 
 
-def get_biggest_bar(dicts_list):
+def get_biggest_bar(bars):
     return max(
-        dicts_list,
+        bars,
         key=lambda x: x['properties']['Attributes']['SeatsCount'])
 
 
-def get_smallest_bar(dicts_list):
+def get_smallest_bar(bars):
     return min(
-        dicts_list,
+        bars,
         key=lambda x: x['properties']['Attributes']['SeatsCount'])
 
 
-def get_closest_bar(dicts_list, longitude, latitude):
-    return min(dicts_list, key=lambda x: math.sqrt(
-        ((float(longitude) - x['geometry']['coordinates'][0]) ** 2) + (
-                (float(latitude) - x['geometry']['coordinates'][1]) ** 2)))
+def get_closest_bar(bars, longitude, latitude):
+    return min(bars, key=lambda x: math.sqrt(
+        ((longitude - x['geometry']['coordinates'][0]) ** 2) + (
+         (latitude - x['geometry']['coordinates'][1]) ** 2)))
+
+
+def get_coordinate_value(coordinate_name):
+    while (1):
+        coordinate_value = input('{}: '.format(coordinate_name))
+        float_coordinate_value = check_coordinate_value(coordinate_value)
+        if float_coordinate_value is None:
+            print("{} введена неверно. Повторите ввод!".format(
+                coordinate_name))
+            continue
+        return float_coordinate_value
+
+
+def check_coordinate_value(coordinate_value):
+    try:
+        float_coordinate_value = float(coordinate_value)
+    except (ValueError, NameError, TypeError):
+        float_coordinate_value = None
+    finally:
+        return float_coordinate_value
 
 
 if __name__ == '__main__':
     args = create_args_parser()
     try:
-        dicts_list = load_data(args.filepath)
+        bars = load_data(args.filepath)
     except json.decoder.JSONDecodeError:
-        print('Файл {} не содержит данных json.'.format(args.filepath))
+        sys.exit('Файл {} не содержит данных json.'.format(args.filepath))
     except IOError:
-        print('Невозможно прочитать данные {}.'.format(args.filepath))
+        sys.exit('Невозможно прочитать данные {}.'.format(args.filepath))
+    if not bars:
+        sys.exit('Программа завершила работу из-за отсутствия данных json.')
     else:
-        if not dicts_list:
-            sys.exit('Программа завершила работу из-за отсутствия данных json.')
-        else:
-            the_biggest_bar = get_biggest_bar(dicts_list)
-            the_smallest_bar = get_smallest_bar(dicts_list)
-            longitude = input('Введите долготу: ')
-            latitude = input('Введите широту: ')
-            try:
-                float_longitude = float(longitude)
-            except (ValueError, NameError):
-                sys.exit('Программа завершила работу из-за некорректно введенной долготы')
-            try:
-                float_latitude = float(latitude)
-            except (ValueError, NameError):
-                sys.exit('Программа завершила работу из-за некорректно введенной широты')
-            the_closest_bar = get_closest_bar(dicts_list, longitude, latitude)
-            print('Самый маленький бар - {}'.format(
-                the_smallest_bar['properties']['Attributes']['Name']))
-            print('Самый большой бар - {}'.format(
-                the_biggest_bar['properties']['Attributes']['Name']))
-            print('Самый близкий бар - {}'.format(
-                the_closest_bar['properties']['Attributes']['Name']))
+        the_biggest_bar = get_biggest_bar(bars)
+        the_smallest_bar = get_smallest_bar(bars)
+        longitude = get_coordinate_value('Долгота')
+        latitude = get_coordinate_value('Широта')
+        the_closest_bar = get_closest_bar(bars, longitude, latitude)
+        print('Самый маленький бар - {}'.format(
+            the_smallest_bar['properties']['Attributes']['Name']))
+        print('Самый большой бар - {}'.format(
+            the_biggest_bar['properties']['Attributes']['Name']))
+        print('Самый близкий бар - {}'.format(
+            the_closest_bar['properties']['Attributes']['Name']))
